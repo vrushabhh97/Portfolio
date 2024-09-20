fe1453bbd0ee6907819f1ba30e705db6d26b2401
Vrushabh Deogirikar <68142086+vrushabhh97@users.noreply.github.com>
2024-09-20 02:16:36 -0700

    config

--- 3d_portfolio/vite.config.js
+++ 3d_portfolio/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './',  // Set the root to the project folder
+  build: {
+    outDir: 'dist',  // Vite will output the built files to this directory
+    rollupOptions: {
+      input: 'index.html',  // Ensure Vite knows to start from index.html in the root
+    },
+  },
   plugins: [react()],
 });
